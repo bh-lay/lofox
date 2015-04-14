27e947961d8c0023b9b7c913f236462670340499
bh-lay <bh_lay@126.com>
2015-04-14 17:24:44 +0800

    制作调皮的demo

--- asset/index.js
+++ asset/index.js
@@ -1,9 +1,22 @@
-var index_tpl = '大家好，我是首页';
-var intro_tpl = '简介';
-var about_tpl = '关于我';
+var index_tpl = '<p>大家好，我是首页</p>';
+var intro_tpl = '<p>简介</p>';
+var about_tpl = '<p>关于我</p>';
 var rest_tpl = 'error';
 var $content = $('.content');
     
+
+function addNew(txt){
+  var last = $content.find('p').last();
+  last.siblings().slideUp(100,function(){
+    $(this).remove();
+  });
+  last.fadeTo(100,0.4).animate({
+    fontSize: 20
+  },100);
+  var newDom = $(txt).hide();
+  $content.append(newDom);
+  newDom.fadeIn(400);
+}
 
 //创建一个lofox实例
 var lofox = new util.lofox();
@@ -13,15 +26,15 @@ var lofox = new util.lofox();
 */
 //首页
 lofox.set('/lofox/index.html',function(){
-    $content.html(index_tpl);
+  addNew(index_tpl);
 });
 //简介
 lofox.set('/lofox/intro.html',function(){
-    $content.html(intro_tpl);
+  addNew(intro_tpl);
 });
 //关于
 lofox.set('/lofox/about.html',function(){
-    $content.html(about_tpl);
+  addNew(about_tpl);
 });
 
 //设置剩余未配置的页面
